7ef850169c6ac27f8b0428b894e3fedc85d73ccb
Carlos Ferreira <carlos@especializati.com.br>
2022-11-21 10:55:14 -0300

    feat: create server

---
+++ server/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+const urlAPI = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+
+type USD_BRL struct {
+	USDBRL struct {
+		Code       string `json:"code"`
+		Codein     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	} `json:"USDBRL"`
+}
+
+func main() {
+  http.HandleFunc("/", GetUSDHandler)
+  http.ListenAndServe(":8080", nil)
+}
+
+func GetUSDHandler(w http.ResponseWriter, r *http.Request) {
+  usd, err := GetPrice()
+  if err != nil {
+    panic(err)
+  }
+
+  json.NewEncoder(w).Encode(usd)
+}
+
+func GetPrice() (*USD_BRL, error) {
+  res, err := http.Get(urlAPI)
+  if err != nil {
+    return nil, err
+  }
+  defer res.Body.Close()
+
+  body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+  log.Print(string(body))
+
+  var usd USD_BRL
+  err = json.Unmarshal(body, &usd)
+  if err != nil {
+    return nil, err
+  }
+  return &usd, nil
+}
